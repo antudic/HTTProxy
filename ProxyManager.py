7d7d7b991cd8622163f6439ef032940df19094f1
Joel <joel@peman.se>
2023-05-13 14:29:24 +0200

    newest

--- ProxyManager.py
+++ ProxyManager.py
@@ -1,11 +1,28 @@
 import ProxyChecker as PC
+import py2sqlite
 import requests
+import asyncio
+import sqlite3
 import time
 
 db = sqlite3.connect("hot.db")
 
 
-class 
+class ProxyValues:
+    # class for managing what proxy values to check for
+
+    latency: float = 9.0 # seconds
+    reliability: float = 0.0 # successful requests divided by total amount of requests
+
+    @staticmethod
+    def outOfProxies():
+        latency /= 0.5 # increase latency leniancy by a factor of 2
+        reliability *= 0.5 # decrease reliability leniancy by a factor of 2
+        # this is the fastest way of doing these operations
+        # don't believe me? -> https://pastebin.com/LjqZqc2d
+        
+
+
 
 
 def createSQLiteTable():
@@ -24,7 +41,14 @@ def createSQLiteTable():
 # 
 # Have the query be defined as a parameter: https://pastebin.com/3NP4wcW9
 def getProxy():
-    query = "SELECT * FROM "
+    proxies = db.execute(
+        f"SELECT * FROM hot WHERE latency<{ProxyValues.latency} AND reliability>{ProxyValues.reliability} ORDER BY lastUsed LIMIT 1;"
+    ).fetchall()
+
+    try:
+        return proxies[0]
+    except Exception:
+
 
 
 def sendRequest(address, data=None):
